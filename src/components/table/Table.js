34f41759c45a0b03d9d237e3bda91814f7727fba
Denny Pearce <digital.y.coffee@gmail.com>
2022-01-20 23:45:19 +0200

    full realize generate table: headers row, numbers rows and cells; start prepare to realize resize logic (add data-attributes)

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,4 +1,6 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
+import { createTable } from './table.template';
+import { $ } from '../../core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -11,38 +13,8 @@ export class Table extends ExcelComponent {
   }
 
   toHTML() {
-    return `
-		<div class="row">
-			<div class="row-info"></div>
-
-			<div class="row-data">
-				<div class="column">A</div>
-				<div class="column">B</div>
-				<div class="column">C</div>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="row-info">1</div>
-
-			<div class="row-data">
-				<div class="cell selected" contenteditable>A1</div>
-				<div class="cell" contenteditable>B1</div>
-				<div class="cell" contenteditable>C1</div>
-			</div>
-		</div>
-
-		<div class="row">
-			<div class="row-info">2</div>
-
-			<div class="row-data">
-				<div class="cell">A2</div>
-				<div class="cell">B2</div>
-				<div class="cell">C2</div>
-			</div>
-		</div>
-	`;
+    return createTable(20);
   }
 
-  onMousedown() {}
+  onMousedown(event) {}
 }
