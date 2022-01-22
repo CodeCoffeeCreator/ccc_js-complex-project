23aa311401fbf68c13986605d72593fca3c3930a
Denny Pearce <digital.y.coffee@gmail.com>
2022-01-22 19:14:17 +0200

    improve performance by go out selector cells search

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -21,14 +21,13 @@ export class Table extends ExcelComponent {
       const $resizer = $(event.target);
       const $parent = $resizer.closest('[data-type="resizable"]');
       const coords = $parent.getCoords();
+      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
 
       document.onmousemove = (e) => {
         const delta = e.pageX - coords.right;
         const value = coords.width + delta;
-        // $parent.$el.style.width = value + 'px';
-        document
-          .querySelectorAll(`[data-col="${$parent.data.col}"]`)
-          .forEach((el) => (el.style.width = value + 'px'));
+        $parent.$el.style.width = value + 'px';
+        cells.forEach((el) => (el.style.width = value + 'px'));
       };
 
       document.onmouseup = () => {
@@ -37,3 +36,17 @@ export class Table extends ExcelComponent {
     }
   }
 }
+
+// 560 мс  Сценарии
+// 4844 мс  Отрисовка
+// 1286 мс  Отображение
+// 1633 мс  Система
+// 4451 мс  Бездействует
+// 12775 мс  Всего
+// -----------------------------------
+// 256 мс  Сценарии
+// 2709 мс  Отрисовка
+// 768 мс  Отображение
+// 994 мс  Система
+// 8569 мс  Бездействует
+// 13296 мс  Всего
