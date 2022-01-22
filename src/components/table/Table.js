5928a3a14d7260f6b4db5818032ef50922108c11
Denny Pearce <digital.y.coffee@gmail.com>
2022-01-23 00:34:45 +0200

    decompose our code to some modules, add stats from render after optimization

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
+import { shouldResize } from './table.functions';
+import { resizeHandler } from './table.resize';
 import { createTable } from './table.template';
-import { $ } from '../../core/dom';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -17,51 +18,8 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target);
-      const $parent = $resizer.closest('[data-type="resizable"]');
-
-      const coords = $parent.getCoords();
-      const type = $resizer.data.resize;
-      const sideProp = type === 'col' ? 'bottom' : 'right';
-      let value;
-
-      $resizer.css({
-        opacity: 1,
-        [sideProp]: '-3000px',
-      });
-
-      document.onmousemove = (e) => {
-        if (type === 'col') {
-          const delta = e.pageX - coords.right;
-          value = coords.width + delta;
-          $resizer.css({ right: -delta + 'px' });
-        } else {
-          const delta = e.pageY - coords.bottom;
-          value = coords.height + delta;
-          $resizer.css({ bottom: -delta + 'px' });
-        }
-      };
-
-      document.onmouseup = () => {
-        document.onmousemove = null;
-        document.onmouseup = null;
-
-        if (type === 'col') {
-          $parent.css({ width: value + 'px' });
-          this.$root
-            .findAll(`[data-col="${$parent.data.col}"]`)
-            .forEach((el) => (el.style.width = value + 'px'));
-        } else {
-          $parent.css({ height: value + 'px' });
-        }
-
-        $resizer.css({
-          opacity: 0,
-          bottom: 0,
-          right: 0,
-        });
-      };
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
     }
   }
 }
@@ -79,3 +37,10 @@ export class Table extends ExcelComponent {
 // 994 мс  Система
 // 8569 мс  Бездействует
 // 13296 мс  Всего
+// -----------------------------------
+// 139 мс  Сценарии
+// 379 мс  Отрисовка
+// 85 мс  Отображение
+// 454 мс  Система
+// 14637 мс  Бездействует
+// 15695 мс  Всего
