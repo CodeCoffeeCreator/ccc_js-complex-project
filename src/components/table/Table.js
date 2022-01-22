2fe314e42593e4767e05af1a94ba0e3f285d442e
Denny Pearce <digital.y.coffee@gmail.com>
2022-01-22 15:41:43 +0200

    realize full columns resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -25,7 +25,10 @@ export class Table extends ExcelComponent {
       document.onmousemove = (e) => {
         const delta = e.pageX - coords.right;
         const value = coords.width + delta;
-        $parent.$el.style.width = value + 'px';
+        // $parent.$el.style.width = value + 'px';
+        document
+          .querySelectorAll(`[data-col="${$parent.data.col}"]`)
+          .forEach((el) => (el.style.width = value + 'px'));
       };
 
       document.onmouseup = () => {
