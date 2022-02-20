21b5113ef2cce9061ba32dd23c8a210905b372d0
Denny Pearce <digital.y.coffee@gmail.com>
2022-02-20 15:12:24 +0200

    change styles trough the store

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -54,7 +54,8 @@ export class Table extends ExcelComponent {
     this.selection.select($cell);
     this.$emit('table:select', $cell);
 
-    console.log($cell.getStyles(Object.keys(defaultStyles)));
+    const styles = $cell.getStyles(Object.keys(defaultStyles));
+    this.$dispatch(actions.changeStyles(styles));
   }
 
   async resizeTable(event) {
